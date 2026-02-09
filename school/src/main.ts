25f6fa6508c4cd4fd4d9fc2e093d98e42b29c71b
yeblee <yebin5127@icloud.com>
2026-02-09 21:57:34 +0900

    commit: main.ts 설정

---
+++ school/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  await app.listen(port);
+  console.log(port + '번 포트에서 서버 실행중입니다.');
+}
+bootstrap();
